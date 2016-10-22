918a1ad6a2c946aed6c2f66dd3a00675981bd904
vip-take <shuhei.taketani@gmail.com>
2016-10-22 21:13:16 +0900

    add show and edit action in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,42 @@ class UsersController < ApplicationController
     find_user
   end
 
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+    if params[:user][:password] == ""
+      @user.update(update_params)
+      if @user.errors.present?
+        flash[error] = @user.errors.full_messages
+        redirect_to :back and return
+      end
+    else
+      @user.update(update_params_with_password)
+      if @user.errors.present?
+        flash[error] = @user.errors.full_messages
+        redirect_to :back and return
+      end
+      redirect_to '/users/sign_in' and return
+    end
+    redirect_to :root, notice: "Profile updated"
+  end
+
+
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def update_params
+    params.require(:user).permit(:email, :f_name, :l_name, :profile, :avatar, :birthday, :gender)
+  end
+
+  def update_params_with_password
+    params.require(:user).permit(:email, :f_name, :l_name, :profile, :avatar, :birthday, :gender, :password , :password_confirmation)
   end
 
 end
