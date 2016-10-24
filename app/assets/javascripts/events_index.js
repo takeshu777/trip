6d5fea3dc0e420512899400126d75a95de96bd18
vip-take <shuhei.taketani@gmail.com>
2016-10-24 12:41:56 +0900

    add navbar dropdown menu javascript .

---
+++ app/assets/javascripts/events_index.js
@@ -0,0 +1,10 @@
+$(function() {
+  $("#js-navbar-dropdown").on('click', function(){
+    var hasClass = $(".header-nav-menu-dropdown-ul").attr("class");
+    if (hasClass.match(/display-none/)) {
+      $(".header-nav-menu-dropdown-ul").removeClass("display-none");
+    } else {
+      $(".header-nav-menu-dropdown-ul").addClass("display-none");
+    }
+  });
+});
