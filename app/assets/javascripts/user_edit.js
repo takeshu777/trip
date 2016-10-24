e42bb01acce499f3aed25a0c4ffcbddb17f19c0c
vip-take <shuhei.taketani@gmail.com>
2016-10-24 12:42:30 +0900

    add profile edit page javascript.

---
+++ app/assets/javascripts/user_edit.js
@@ -0,0 +1,21 @@
+$(function () {
+  //event_lister
+  $(".useredit__container-menu-li-text").on('click', function(ev) {
+    // 選択項目のハイライトリセット
+    $(".useredit__container-menu-li").removeClass("useredit__container-menu-li-active");
+    // 選択項目をハイライト
+    $(this).parent().addClass("useredit__container-menu-li-active");
+
+    // 表示項目のリセット
+    $(".useredit__container-form-list").children().addClass("display-none");
+
+    // 項目の表示
+    if (ev.target.text == "プロフィール設定") {
+      $(".useredit__container-form-list-profile").removeClass("display-none");
+    } else if (ev.target.text == "アカウント設定") {
+      $(".useredit__container-form-list-account").removeClass("display-none");
+    } else if (ev.target.text == "パスワード設定") {
+      $(".useredit__container-form-list-password").removeClass("display-none");
+    }
+  });
+});
