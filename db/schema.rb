ab13927cd2356e96309c4884fda3243ca6bab2bd
vip-take <shuhei.taketani@gmail.com>
2016-10-25 21:30:57 +0900

    comit db/schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024051358) do
+ActiveRecord::Schema.define(version: 20161025015043) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20161024051358) do
     t.integer  "user_id",    limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image",      limit: 255
+    t.string   "dest",       limit: 255
   end
 
   create_table "users", force: :cascade do |t|
