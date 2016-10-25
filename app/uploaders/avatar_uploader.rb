e0d6fa8e7614ac5eb8f6d5c2bcf6fce5fc3074e5
vip-take <shuhei.taketani@gmail.com>
2016-10-26 00:50:41 +0900

    set default image in carrierwave.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -21,6 +21,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+     "/fallback/" + [version_name, "events_sample.jpg"].compact.join('_')
+  end
+
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
