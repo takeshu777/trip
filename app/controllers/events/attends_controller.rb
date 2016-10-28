f727bc7098337c5fc9d92765794e4b1597e4a982
vip-take <shuhei.taketani@gmail.com>
2016-10-28 18:21:36 +0900

    create attends controller

---
+++ app/controllers/events/attends_controller.rb
@@ -0,0 +1,2 @@
+class Events::AttendsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028085512) do
+ActiveRecord::Schema.define(version: 20161028090508) do
+
+  create_table "attends", force: :cascade do |t|
+    t.integer  "event_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.text     "comment",    limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title",            limit: 255
