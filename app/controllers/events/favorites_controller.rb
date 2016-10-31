a0904fb867ab9eb9e4690b73bab4288486770e64
vip-take <shuhei.taketani@gmail.com>
2016-10-31 15:57:06 +0900

    create favorites controller

--- app/controllers/events/favorites_controller.rb
+++ app/controllers/events/favorites_controller.rb
@@ -1,2 +1,18 @@
 class Events::FavoritesController < ApplicationController
+
+  def create
+    Favorite.create(create_params)
+    @event = Event.find(params[:event_id])
+  end
+
+  def destroy
+    Favorite.find(params[:id]).destroy
+    @event = Event.find(params[:event_id])
+  end
+
+  private
+  def create_params
+    params.permit(:event_id).merge(user_id: current_user.id)
+  end
+
 end
