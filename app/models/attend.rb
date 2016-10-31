8ac325473e2df89e3a524a4e06f07d705040cfdf
vip-take <shuhei.taketani@gmail.com>
2016-10-31 09:57:26 +0900

    add favorite and attend count to event model

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -1,6 +1,6 @@
 class Attend < ActiveRecord::Base
 
-  belongs_to :event
+  belongs_to :event, :counter_cache => true
   belongs_to :user, :counter_cache => true
 
   validates :comment, presence: true
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ActiveRecord::Base
 
-  belongs_to :event
+  belongs_to :event, :counter_cache => true
   belongs_to :user, :counter_cache => true
 
 end
