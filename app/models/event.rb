397a0f965d81a931a5b12b9a27675eac7ef16989
vip-take <shuhei.taketani@gmail.com>
2016-11-01 16:52:11 +0900

    add search method in events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,4 +11,20 @@ class Event < ActiveRecord::Base
 
   enum status: { open: 0, draft: 1 }
 
+  # titleとsummaryカラムの部分検索
+  scope :text_like, -> text {
+    where('title like ? or summary like ?', "%#{text}%", "%#{text}%") if text.present?
+  }
+
+  # 開催日の検索
+  scope :date_between, -> from, to {
+    if from.present? && to.present?
+      where(start_date: from..to)
+    elsif from.present?
+      where('start_date >= ?', from)
+    elsif to.present?
+      where('start_date <= ?', to)
+    end
+  }
+
 end
