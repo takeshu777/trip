9447c11550c7ea12724890a6739e3de44f3440ae
vip-take <shuhei.taketani@gmail.com>
2016-11-03 12:13:31 +0900

    add slick js.

--- app/assets/javascripts/events_index_EventsList.js
+++ app/assets/javascripts/events_index_EventsList.js
@@ -3,4 +3,24 @@ $(function() {
   $(".event__container-sort-now").on('click', function(){
     $(".event__container-sort-dropdown-items").toggleClass("display-none");
   });
+
+  // スライダー表示
+    $('.top__container-banner-box').slick({
+      autoplay:true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      asNavFor: '.top__container-banner-campaign-list'
+    });
+
+    $('.top__container-banner-campaign-list').slick({
+      autoplay:true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.top__container-banner-box',
+      centerMode: true,
+      focusOnSelect: true
+    });
+
 });
