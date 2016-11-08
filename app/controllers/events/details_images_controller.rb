df56f63234822274d700eced697618b1e26445cf
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-08 10:17:16 +0900

    delete unnecessary line details_images controller.

--- app/controllers/events/details_images_controller.rb
+++ app/controllers/events/details_images_controller.rb
@@ -7,11 +7,9 @@ class Events::DetailsImagesController < ApplicationController
       format.js {
         @images = DetailsImage.new(create_params)
         if @images.save
-          @file_msg = "画像アップロード完了"
           @event = Event.find(params[:event_id])
         else
           @event = Event.find(params[:event_id])
-          @file_msg = "画像のアップロード失敗"
         end
       }
     end
