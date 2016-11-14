67e1a5478e440d13fda0df735900ca793c207bb0
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-14 12:11:00 +0900

    test commit.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -28,6 +28,7 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
 
-before_exec do |server|
+before_exec do |server, worker|
   ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', working_directory)
+	ENV["SECRET_KEY_BASE"] = <%= ENV["SECRET_KEY_BASE"] %>
 end
