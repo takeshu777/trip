3baed0f188a71082956462d4644dac5e65391bd2
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-17 16:17:08 +0900

    edit set :branch value

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :repo_url, 'git@github.com:/vip-take/trip.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :branch, 'master'
+set :branch, ENV["BRANCH"]
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/home/trip_admin/trip'
