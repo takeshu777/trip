23b47323a989b5f6571ae74a76aefc74650ff1fe
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-19 14:29:11 +0900

    edit line_controller.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -12,29 +12,31 @@ class LineController < ApplicationController
 	end
 
 	def callback
-		# リクエストの内容を取得
-	  body = request.body.read
+	  respond_to do |format|
+	    format.json {
+				# リクエストの内容を取得
+			  body = request.body.read
 
-	  # LINEからのリクエストが正しいかどうかの確認
-	  signature = request.env['HTTP_X_LINE_SIGNATURE']
-	  unless client.validate_signature(body, signature)
-	    error 400 do 'Bad Request' end
-	  end
+			  # LINEからのリクエストが正しいかどうかの確認
+			  signature = request.env['HTTP_X_LINE_SIGNATURE']
+			  unless client.validate_signature(body, signature)
+			    error 400 do 'Bad Request' end
+			  end
 
-	  events = client.parse_events_from(body)
-	  events.each { |event|
-	    case event
-	    when Line::Bot::Event::Message
-	      case event.type
-	      when Line::Bot::Event::MessageType::Text
-	        message = {
-	          type: 'text',
-	          text: "hoge"
-	        }
-	        client.reply_message(event['replyToken'], message)
-	    end
-	  }
-
+			  events = client.parse_events_from(body)
+			  events.each { |event|
+			    case event
+			    when Line::Bot::Event::Message
+			      case event.type
+			      when Line::Bot::Event::MessageType::Text
+			        message = {
+			          type: 'text',
+			          text: "hoge"
+			        }
+			        client.reply_message(event['replyToken'], message)
+			    end
+			  }
+	    }
+		end
 	end
-
 end
