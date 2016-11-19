896d5280801cdcaa9e64972b8602dbca8f346807
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-19 12:09:30 +0900

    edit line callback action.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,6 +1,38 @@
 class LineController < ApplicationController
 
 	def callback
+		# リクエストの内容を取得
+	  body = request.body.read
+
+	  # LINEからのリクエストが正しいかどうかの確認
+	  signature = request.env['HTTP_X_LINE_SIGNATURE']
+	  unless client.validate_signature(body, signature)
+	    error 400 do 'Bad Request' end
+	  end
+
+	  events = client.parse_events_from(body)
+	  events.each { |event|
+	    case event
+	    when Line::Bot::Event::Message
+	      case event.type
+	      when Line::Bot::Event::MessageType::Text
+	        message = {
+	          type: 'text',
+	          text: event.message['text']
+	        }
+	        client.reply_message(event['replyToken'], message)
+	    end
+	  }
+
+	  "OK"
+
 	end
 
+private
+	def client
+	  @client ||= Line::Bot::Client.new { |config|
+	    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+	    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+	  }
+	end
 end
