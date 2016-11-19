6ba3ea8006d3465b973345a2a130d4439dbd2eea
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-19 14:42:52 +0900

    edit linecontroller.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -12,31 +12,90 @@ class LineController < ApplicationController
 	end
 
 	def callback
-	  respond_to do |format|
-	    format.json {
-				# リクエストの内容を取得
-			  body = request.body.read
+    reply_base_url = "https://api.line.me/v2/bot/message/reply"
 
-			  # LINEからのリクエストが正しいかどうかの確認
-			  signature = request.env['HTTP_X_LINE_SIGNATURE']
-			  unless client.validate_signature(body, signature)
-			    error 400 do 'Bad Request' end
-			  end
+		#リクエストの内容を取得
+		body = request.body.read
 
-			  events = client.parse_events_from(body)
-			  events.each { |event|
-			    case event
-			    when Line::Bot::Event::Message
-			      case event.type
-			      when Line::Bot::Event::MessageType::Text
-			        message = {
-			          type: 'text',
-			          text: "hoge"
-			        }
-			        client.reply_message(event['replyToken'], message)
-			    end
-			  }
-	    }
-		end
+		#replyTokenの取得
+	  events = client.parse_events_from(body)
+	  events.each { |event|
+	  	replyToken = event['replyToken']
+	  }
+
+    # APIとの通信に必要な認証情報
+    channel_access_token = ENV['LINE_ACCESS_TOKEN']
+    channel_secret = ENV['LINE_CHANNEL_SECRET']
+
+    # リクエストURIを解析
+    uri = URI.parse(base_url)
+
+    # リクエスト生成
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    # 通信はSSL(HTTPS)
+    http.use_ssl = true
+
+    # ポストで投げる
+    req = Net::HTTP::Post.new(uri.path)
+
+    @event_list = Event.all.limit(5)
+
+    # ヘッダーの定義
+    req["Content-Type"] = "application/json; charser=UTF-8"
+    req["Authorization"] = "Bearer " + channel_access_token
+
+    # carouselデータ配列の作成
+    @carousel_data_list = []
+
+    @event_list.each do |event|
+      # メイン画像が未登録の場合はデフォルト画像をサムネイルurlにセット
+      if event.image.present?
+        @carousel_data = {
+          "thumbnailImageUrl": "#{event.image}",
+          "title": "#{event.title}",
+          "text": "#{event.summary}",
+          "actions": [
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "https://training-not-trippiece.ienikki.com/events/#{event.id}"
+              }
+          ]
+        }
+      else
+        @carousel_data = {
+          "thumbnailImageUrl": "https://training-not-trippiece.ienikki.com#{event.image}",
+          "title": "#{event.title}",
+          "text": "#{event.summary}",
+          "actions": [
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "https://training-not-trippiece.ienikki.com/events/#{event.id}"
+              }
+          ]
+        }
+      end
+      @carousel_data_list << @carousel_data
+    end
+
+    # 投げるデータの定義
+    data = {
+    "type": "template",
+    "altText": "this is a carousel template",
+    "template": {
+        "type": "carousel",
+        "columns": @carousel_data_list
+      }
+    }
+
+    data_array = [data]
+
+    payload = { "replyToken" => @replyToken, "messages"  => data_array }.to_json
+
+    req.body = payload
+
+    res = http.request(req)
 	end
 end
