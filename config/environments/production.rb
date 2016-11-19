61e07da5e201d927ed9c44fb3d2722b936c2fb35
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-19 11:43:31 +0900

    delete unnecessary line.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,6 @@ Rails.application.configure do
   config.assets.paths << "/home/trip_admin/trip/app/vendor/assets/javascripts"
   config.assets.paths << "/home/trip_admin/trip/app/vendor/assets/stylesheets"
 
-  config.autoload_paths += %W(#{config.root}/lib)
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
