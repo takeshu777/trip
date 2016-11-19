0a4fd374c669953dc19d7a6b3f30d19bb99b41ae
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-19 14:21:58 +0900

    edit line_controller

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -20,5 +20,21 @@ class LineController < ApplicationController
 	  unless client.validate_signature(body, signature)
 	    error 400 do 'Bad Request' end
 	  end
+
+	  events = client.parse_events_from(body)
+	  events.each { |event|
+	    case event
+	    when Line::Bot::Event::Message
+	      case event.type
+	      when Line::Bot::Event::MessageType::Text
+	        message = {
+	          type: 'text',
+	          text: "hoge"
+	        }
+	        client.reply_message(event['replyToken'], message)
+	    end
+	  }
+
 	end
+
 end
