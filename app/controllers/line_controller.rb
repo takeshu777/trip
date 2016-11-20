f60c214b46a4622836c34fa5f93f905ad352eec4
Shuhei Taketani <staketani@Takeshu-Private.local>
2016-11-20 23:25:14 +0900

    edit line_cont.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -9,41 +9,10 @@ class LineController < ApplicationController
 		@log = Logger.new('log/hogehoge.log')
 
     # APIとの通信に必要な認証情報
-    @channel_access_token = ENV['LINE_ACCESS_TOKEN']
-    @channel_secret = ENV['LINE_CHANNEL_SECRET']
+    channel_access_token = ENV['LINE_ACCESS_TOKEN']
+    channel_secret = ENV['LINE_CHANNEL_SECRET']
 
-		#リクエストの内容を取得。
-		body = request.body.read
-
-		#リクエストボディの取り出し
-	  events = client.parse_events_from(body)
-
-	  # bodyの出力。array型
-		@log.info(events)
-
-		@log.info(postback.data)
-
-	  events.each { |event|
-	  	@log.info(event)
-	  	@replyToken = event['replyToken']
-	  	@callback_type = event['type']
-	  }
-
-	  case @callback_type
-	  when "message"
-			reply_near_apply_date
-			@log.info("okay")
-		when "follow", "join"
-			reply_welcome
-		end
-
-
-	end
-
-	private
-
-	def reply_welcome
-		#push通知用URL
+		#返信通知URL
     base_url = "https://api.line.me/v2/bot/message/reply"
 
     # リクエストURIを解析
@@ -60,20 +29,68 @@ class LineController < ApplicationController
 
     # ヘッダーの定義
     req["Content-Type"] = "application/json; charser=UTF-8"
-    req["Authorization"] = "Bearer " + @channel_access_token
+    req["Authorization"] = "Bearer " + channel_access_token
 
-    # 投げるデータの定義
+		#リクエストの内容を取得。
+		body = request.body.read
+
+		#リクエストボディの取り出し
+	  events = client.parse_events_from(body)
+
+	  # bodyの出力。array型
+		@log.info(events)
+
+		# リクエスト情報の取り出し
+	  events.each { |event|
+	  	@log.info(event)
+	  	@replyToken = event['replyToken']
+	  	@callback_type = event['type']
+	  	@postback_data = event['postback']['data']	if @callback_type == "postback"
+	  	@message_data = event['message']	if @callback_type == "message"
+	  }
+
+	  #返信data（メッセージ部分）の作成
+    @data_array = []
+
+	  #リクエスト別のアクション定義
+	  case @callback_type
+		when "follow", "join"
+			@data_array << follow_join_msg
+			@data_array << default_msg
+		when "postback"
+			@data_array << view_postback_events(@postback_data)
+			@data_array << default_msg
+	  when "message"
+			@data_array << view_search_events(@message_data)
+			@data_array << default_msg
+		end
+
+    payload = { "replyToken" => @replyToken, "messages"  => @data_array }.to_json
+
+    req.body = payload
+
+    res = http.request(req)
+
+    render :nothing => true, status: :ok
+	end
+
+	private
+
+	def follow_join_msg
     data = {
 	    "type": "text",
 	    "text": "初めましてTripPieceです！楽しい企画が沢山あるよーん(^^)/"
     }
+		return data
+	end
 
-    data2 = {
+	def default_msg
+    data = {
 		  "type": "template",
 		  "altText": "this is a buttons template",
 		  "template": {
 		      "type": "buttons",
-		      "text": "どんな企画があるか調べるね。ボタンを押すか、キーワードを打ってね。",
+		      "text": "どんな企画があるか調べる場合はボタンを押すか、キーワードを打ってね。",
 		      "actions": [
 		          {
 		            "type": "postback",
@@ -90,50 +107,25 @@ class LineController < ApplicationController
 		      ]
 		  }
 		}
-
-    data_array = [data,data2]
-
-    payload = { "replyToken" => @replyToken, "messages"  => data_array }.to_json
-
-    req.body = payload
-
-    res = http.request(req)
-
-    render :nothing => true, status: :ok
+		return data
 	end
 
+	def view_postback_events(postback_data)
+	  @log.info(postback_data)
 
-	# 申込日が近い企画のpush
-	def reply_near_apply_date
-
-		#push通知用URL
-    base_url = "https://api.line.me/v2/bot/message/reply"
-
-    # リクエストURIを解析
-    uri = URI.parse(base_url)
-
-    # リクエスト生成
-    http = Net::HTTP.new(uri.host, uri.port)
-
-    # 通信はSSL(HTTPS)
-    http.use_ssl = true
-
-    # ポストで投げる
-    req = Net::HTTP::Post.new(uri.path)
-
-    @event_list = Event.apply_end_date_between(Time.now,"").order(:apply_end_date).open.limit(5)
-
-    # ヘッダーの定義
-    req["Content-Type"] = "application/json; charser=UTF-8"
-    req["Authorization"] = "Bearer " + @channel_access_token
+		if postback_data == 'action=view&name=near'
+	    event_list = Event.apply_end_date_between(Time.now,"").order(:apply_end_date).open.limit(5)
+		elsif postback_data == 'action=view&name=new'
+			event_list = Event.order('id DESC').open.limit(5)
+		end
 
     # carouselデータ配列の作成
-    @carousel_data_list = []
+    carousel_data_list = []
 
-    @event_list.each do |event|
+    event_list.each do |event|
       # メイン画像が未登録の場合はデフォルト画像をサムネイルurlにセット
       if event.image.present?
-        @carousel_data = {
+        carousel_data = {
           "thumbnailImageUrl": "#{event.image}",
           "title": "#{event.title}",
           "text": "#{event.summary}",
@@ -146,7 +138,7 @@ class LineController < ApplicationController
           ]
         }
       else
-        @carousel_data = {
+        carousel_data = {
           "thumbnailImageUrl": "https://training-not-trippiece.ienikki.com#{event.image}",
           "title": "#{event.title}",
           "text": "#{event.summary}",
@@ -159,7 +151,7 @@ class LineController < ApplicationController
           ]
         }
       end
-      @carousel_data_list << @carousel_data
+      carousel_data_list << carousel_data
     end
 
     # 投げるデータの定義
@@ -168,19 +160,62 @@ class LineController < ApplicationController
     "altText": "this is a carousel template",
     "template": {
         "type": "carousel",
-        "columns": @carousel_data_list
+        "columns": carousel_data_list
       }
     }
+    return data
+	end
 
-    data_array = [data]
 
-    payload = { "replyToken" => @replyToken, "messages"  => data_array }.to_json
+	def view_search_events(message_data)
+		@log.info(message_data)
+	  event_list = Event.text_like(params[:text]).date_between(params[:start_date],params[:end_date]).open.limit(5)
 
-    req.body = payload
+    # carouselデータ配列の作成
+    carousel_data_list = []
 
-    res = http.request(req)
+    event_list.each do |event|
+      # メイン画像が未登録の場合はデフォルト画像をサムネイルurlにセット
+      if event.image.present?
+        carousel_data = {
+          "thumbnailImageUrl": "#{event.image}",
+          "title": "#{event.title}",
+          "text": "#{event.summary}",
+          "actions": [
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "https://training-not-trippiece.ienikki.com/events/#{event.id}"
+              }
+          ]
+        }
+      else
+        carousel_data = {
+          "thumbnailImageUrl": "https://training-not-trippiece.ienikki.com#{event.image}",
+          "title": "#{event.title}",
+          "text": "#{event.summary}",
+          "actions": [
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "https://training-not-trippiece.ienikki.com/events/#{event.id}"
+              }
+          ]
+        }
+      end
+      carousel_data_list << carousel_data
+    end
 
-    render :nothing => true, status: :ok
+    # 投げるデータの定義
+    data = {
+    "type": "template",
+    "altText": "this is a carousel template",
+    "template": {
+        "type": "carousel",
+        "columns": carousel_data_list
+      }
+    }
+    return data
 	end
 
 	def client
